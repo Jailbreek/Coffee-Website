eec17228628b8ba7113e551032d5c257557c7519
cpt_n3m0 <wijayaandre769@gmail.com>
2023-03-29 23:10:47 +0700

    update contact us feature

--- js/script.js
+++ js/script.js
@@ -13,3 +13,34 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+function handleSubmit(event) {
+  event.preventDefault(); // Prevents the form from submitting normally
+
+  // Get the input values from the form
+  const name = document.querySelector('input[placeholder="Name"]').value;
+  const email = document.querySelector('input[placeholder="Email"]').value;
+  const phone = document.querySelector(
+    'input[placeholder="Phone Number"]'
+  ).value;
+
+  // Check if all fields are filled in
+  if (!name || !email || !phone) {
+    alert("Please fill all fields.");
+    return;
+  }
+
+  // Do something with the input values, e.g. send them to a server
+  console.log(`Name: ${name}\nEmail: ${email}\nPhone Number: ${phone}`);
+
+  // Display the input values in a pop-up message
+  const message = `Name: ${name}\nEmail: ${email}\nPhone Number: ${phone}`;
+  alert(message);
+
+  // Reset the form after submitting
+  event.target.reset();
+}
+
+// Attach the handleSubmit function to the form's submit event
+const form = document.querySelector("form");
+form.addEventListener("submit", handleSubmit);
